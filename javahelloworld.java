5912b72a1bdc2b61cd14f30f584cd8a90be2db02
ubuntu963741 <b24egx5byv@dan7u.anonbox.net>
2016-10-06 14:24:47 +0200

    Nuevo cambio

--- javahelloworld.java
+++ javahelloworld.java
@@ -2,6 +2,6 @@ class javahelloworld{
 
 	public static void main (String[] args){
 		System.out.println("Java Hello World");
-		System.out.println("Hello again");
+		System.out.println("Ahí estás...");
 	}
 }
